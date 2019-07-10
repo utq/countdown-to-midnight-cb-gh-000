4e30ed6463f910dd946fb528dc983550fa9316cd
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 20:25:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x.times do
+    puts"#{number} SECOND(S)!"
+  end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (x)
+  x.times do
+    puts"#{number} SECOND(S)!"
+    sleep(1)
+  end
+"HAPPY NEW YEAR!"
 end
